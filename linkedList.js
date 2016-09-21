4cf8d1d0410fd41286a1e92dcae4d0b8a9414e8c
Kumar Bangera <sharathbangera@avaya.com>
2016-09-21 12:10:21 +0530

    formatted Linked List

--- linkedList.js
+++ linkedList.js
@@ -1,60 +1,60 @@
-function linkedList(){
-	this.head=null;
-  this.tail=null;
-  this._length = 0;
+function linkedList() {
+    this.head = null;
+    this.tail = null;
+    this._length = 0;
 }
 
-linkedList.prototype.add = function(newItem){
-	var newNode = {
-  	data : newItem,
-    next : null
-  }
-	if(this.head == null){
-  	this.head = newNode;
-    this.tail = newNode;
-  }
-  else{
-  	this.tail.next = newNode;
-    this.tail = newNode;
-  }
-  this._length++;
+linkedList.prototype.add = function(newItem) {
+    var newNode = {
+        data: newItem,
+        next: null
+    }
+    if (this.head == null) {
+        this.head = newNode;
+        this.tail = newNode;
+    } else {
+        this.tail.next = newNode;
+        this.tail = newNode;
+    }
+    this._length++;
 }
 
-linkedList.prototype.print = function(){
-	console.log(this.head);
+linkedList.prototype.print = function() {
+    console.log(this.head);
 }
 
-linkedList.prototype.createLoop = function(){
-	this.tail.next = this.head;
+linkedList.prototype.createLoop = function() {
+    this.tail.next = this.head;
 }
 
-linkedList.prototype.detectLoop = function(){
-	 var first = this.head;
-   
-	 if(first == null) // list does not exist..so no loop either.
+linkedList.prototype.detectLoop = function() {
+    var first = this.head;
+
+    if (first == null) // list does not exist..so no loop either.
         return false;
 
     var slow, fast; // create two references.
 
     slow = fast = first; // make both refer to the start of the list.
 
-    while(fast != null && fast.next != null) {
+    while (fast != null && fast.next != null) {
 
-        slow = slow.next;          // 1 hop.
+        slow = slow.next; // 1 hop.
         fast = fast.next.next; // 2 hops.
-        
-        if(slow == fast) // if the two ever meet...we have a loop.
+
+        if (slow == fast) // if the two ever meet...we have a loop.
             return true;
     }
     return false;
 }
 
-
-linkedList.prototype.add(1);
-linkedList.prototype.add(10);
-linkedList.prototype.add(100);
-linkedList.prototype.add(1000);
-linkedList.prototype.print();
-linkedList.prototype.createLoop();
-linkedList.prototype.print();
-console.log(linkedList.prototype.detectLoop());
+var link = new linkedList();
+link.add(1);
+link.add(10);
+link.add(100);
+link.add(1000);
+link.print();
+link.createLoop();
+link.print();
+console.log(link.detectLoop());
+console.log(link._length);
